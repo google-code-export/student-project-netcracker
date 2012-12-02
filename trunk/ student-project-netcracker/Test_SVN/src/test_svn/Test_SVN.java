a446bf5d1ecf0e210168b3e04d5070431cf5e07b
troxim@gmail.com <troxim@gmail.com@26810a32-ff7d-2d67-9415-e6df546b8706>
2012-12-02 11:58:51 +0000

    It`s test class

---
+++ trunk/ student-project-netcracker/Test_SVN/src/test_svn/Test_SVN.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test_svn;
+
+/**
+ *
+ * @author bidnyarskiy
+ */
+public class Test_SVN {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("FFFFFFF");
+    }
+}
