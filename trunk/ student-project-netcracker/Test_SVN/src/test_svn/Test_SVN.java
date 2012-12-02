d1e7aa05d0e893a1bd8bd6a9d25351b3bfe33847
troxim@gmail.com <troxim@gmail.com@26810a32-ff7d-2d67-9415-e6df546b8706>
2012-12-02 12:00:34 +0000

    Commit my change

--- trunk/ student-project-netcracker/Test_SVN/src/test_svn/Test_SVN.java
+++ trunk/ student-project-netcracker/Test_SVN/src/test_svn/Test_SVN.java
@@ -14,6 +14,6 @@ public class Test_SVN {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("FFFFFFF");
+        System.out.println("FFFFFFF_________");
     }
 }
