e8b5f1f25f4cc8bf3979c99955014f8fadecfdc7
troxim@gmail.com <troxim@gmail.com@26810a32-ff7d-2d67-9415-e6df546b8706>
2012-12-02 12:12:46 +0000

    Commit my change to shureg

--- trunk/ student-project-netcracker/Test_SVN/src/test_svn/Test_SVN.java
+++ trunk/ student-project-netcracker/Test_SVN/src/test_svn/Test_SVN.java
@@ -15,5 +15,6 @@ public class Test_SVN {
      */
     public static void main(String[] args) {
         System.out.println("FFFFFFF_________SHUREG TEST CHANGE");
+        System.out.println("fffffff");
     }
 }
