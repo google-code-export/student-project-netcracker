3bcc65768e780320203f2e9b95b7aea230b69ac6
klitna.tetiana@gmail.com <klitna.tetiana@gmail.com@26810a32-ff7d-2d67-9415-e6df546b8706>
2013-01-20 11:48:09 +0000

    update project

---
+++ Interview/src/main/java/ua/netcrackerteam/reports/TypesOfViewReport.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ua.netcrackerteam.reports;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author tanya
+ */
+public interface TypesOfViewReport {
+     /**
+     * Reflection of information of report
+     * @param dataForView 
+     */
+    public void viewReport(ArrayList dataForView);
+}
