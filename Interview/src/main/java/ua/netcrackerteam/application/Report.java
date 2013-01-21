a930005016d2acc1dd799fb7a36f10962b675aab
klitna.tetiana@gmail.com <klitna.tetiana@gmail.com@26810a32-ff7d-2d67-9415-e6df546b8706>
2013-01-21 18:14:47 +0000

    update project

--- Interview/src/main/java/ua/netcrackerteam/application/Report.java
+++ Interview/src/main/java/ua/netcrackerteam/application/Report.java
@@ -2,10 +2,9 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package ua.netcrackerteam.reports;
+package ua.netcrackerteam.application;
 
 import java.util.ArrayList;
-import ua.netcrackerteam.application.*;
 
 /**
  *
