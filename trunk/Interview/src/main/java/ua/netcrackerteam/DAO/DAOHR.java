9937cccc298c3b6f504499cd821338ac62d42c2a
filipenko.aleksey@gmail.com <filipenko.aleksey@gmail.com@26810a32-ff7d-2d67-9415-e6df546b8706>
2013-01-22 07:14:46 +0000

    New class of methods for users with roles admin and HR

---
+++ trunk/Interview/src/main/java/ua/netcrackerteam/DAO/DAOHR.java
@@ -0,0 +1,13 @@
+package ua.netcrackerteam.DAO;
+
+import ua.netcrackerteam.configuration.Logable;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Bri
+ * Date: 22.01.13
+ * Time: 9:10
+ * To change this template use File | Settings | File Templates.
+ */
+public class DAOHR implements Logable {
+}
