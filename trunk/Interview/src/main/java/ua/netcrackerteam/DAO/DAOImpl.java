b579fa4d0a47f17c4a52406f013ba56313a2ee76
krygina.ua <krygina.ua@26810a32-ff7d-2d67-9415-e6df546b8706>
2013-01-29 13:24:09 +0000

    Next step in understanding dao

--- trunk/Interview/src/main/java/ua/netcrackerteam/DAO/DAOImpl.java
+++ trunk/Interview/src/main/java/ua/netcrackerteam/DAO/DAOImpl.java
@@ -5,8 +5,10 @@ import org.hibernate.Session;
 import ua.netcrackerteam.configuration.HibernateFactory;
 import ua.netcrackerteam.configuration.HibernateUtil;
 import ua.netcrackerteam.configuration.Logable;
+
 import java.sql.SQLException;
-import java.util.*;
+import java.util.List;
+import java.util.Locale;
 
 /**
  * @author
@@ -21,7 +23,7 @@ public class DAOImpl implements DAOInterface, Logable {
             Locale.setDefault(Locale.ENGLISH);
             session = HibernateUtil.getSessionFactory().getCurrentSession();
             session.beginTransaction();
-            re = session.createQuery("select cc, c, f from TableContact cc join cc.contact c join cc.idForm f");
+            re = session.createQuery("from TableContact");
             listOfForms = re.list();
         } catch (Exception e) {
             System.out.println(e);
@@ -35,17 +37,21 @@ public class DAOImpl implements DAOInterface, Logable {
 
     public static void main (String[] args) throws SQLException{
         Locale.setDefault(Locale.ENGLISH);
-        List<Object[]> listOfForms = null;
         System.out.println("OH MY GGGGGGGGGGGGOOOOOOOOOOOOD!!!! THIS IS SPARTANS FIRST QUERY !!!!");
         System.out.println("=====================================================================");
         try{
-            listOfForms = HibernateFactory.getInstance().getFormDAO().GetNamesAndContacts();
-            for (Object[] currForm : listOfForms) {
+            List<TableContact> listOfForms = HibernateFactory.getInstance().getFormDAO().GetNamesAndContacts();
+            for (TableContact currForm : listOfForms) {
+                System.out.println(currForm.getInfo()+"  "+currForm.getContact().getCategory()+"  "+currForm.getIdForm().getFirstName());
+            }
+
+            /*for (Object[] currForm : listOfForms) {
+                String[] dsfsdf = (String[])currForm;
                 TableContact contactObjects = (TableContact) currForm[0];
                 TableContactCategory contactCategoryObjects = (TableContactCategory) currForm[1];
                 TableForm formObjects = (TableForm) currForm[2];
-                System.out.println(contactObjects.getInfo() + " " + contactCategoryObjects.getCategory() + " " +formObjects.getLastName());
-            }
+                System.out.println(dsfsdf[0] + " " + dsfsdf[1] + " " +dsfsdf[2]);
+            }*/
         } catch (Exception e) {
             System.out.println(e);
         }
