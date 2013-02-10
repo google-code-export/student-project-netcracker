41638bd60548efa63f6a25ff4828adb2b2887458
klitna.tetiana@gmail.com <klitna.tetiana@gmail.com@26810a32-ff7d-2d67-9415-e6df546b8706>
2013-02-10 19:54:37 +0000

    Add controller for update intervew for form

--- Interview/src/main/java/ua/netcrackerteam/controller/RegistrationToInterview.java
+++ Interview/src/main/java/ua/netcrackerteam/controller/RegistrationToInterview.java
@@ -4,10 +4,20 @@
  */
 package ua.netcrackerteam.controller;
 
+import ua.netcrackerteam.DAO.Form;
+import ua.netcrackerteam.configuration.HibernateFactory;
+
 /**
  *
  * @author home
  */
 public class RegistrationToInterview {
     
+        public void updateRegistrationToInterview(String userName, Long idInterview) {
+         
+        Form form = HibernateFactory.getInstance().getStudentDAO().getFormByUseLogin(userName);       
+        form.setIdInterview(idInterview);
+        HibernateFactory.getInstance().getStudentDAO().updateForm(form);       
+             
+    }
 }
