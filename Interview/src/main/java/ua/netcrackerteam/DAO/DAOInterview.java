f1d360ad9d58d63fecd7173638f0bb646f270498
klitna.tetiana@gmail.com <klitna.tetiana@gmail.com@26810a32-ff7d-2d67-9415-e6df546b8706>
2013-02-10 19:03:30 +0000

    Add dao for update intervew for form

---
+++ Interview/src/main/java/ua/netcrackerteam/DAO/DAOInterview.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ua.netcrackerteam.DAO;
+
+/**
+ *
+ * @author home
+ */
+public interface DAOInterview {
+    public void updateRegistrationToInterview(Long idUser, Long idInterview);
+   
+}
