13105e47943458ed0108eb5f6846d832f3ea4004
klitna.tetiana@gmail.com <klitna.tetiana@gmail.com@26810a32-ff7d-2d67-9415-e6df546b8706>
2013-04-03 19:24:40 +0000

    Create reports for HR

--- Interview/src/main/java/ua/netcrackerteam/applicationForm/Reports/TypeReports/ReportTemplateAmountStudentCourse.java
+++ Interview/src/main/java/ua/netcrackerteam/applicationForm/Reports/TypeReports/ReportTemplateAmountStudentCourse.java
@@ -7,9 +7,15 @@ package ua.netcrackerteam.applicationForm.Reports.TypeReports;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.pdf.PdfPCell;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import ua.netcrackerteam.DAO.DAOReport;
+import ua.netcrackerteam.DAO.Entities.Institute;
+import ua.netcrackerteam.applicationForm.Reports.Elements.Chart;
 import ua.netcrackerteam.applicationForm.Reports.Elements.DesignTable;
 import ua.netcrackerteam.applicationForm.Reports.Elements.DesignTableWithGroups;
 import ua.netcrackerteam.applicationForm.Reports.ReportTemplateBuilder;
@@ -19,7 +25,20 @@ import ua.netcrackerteam.applicationForm.Reports.ReportTemplateBuilder;
  * @author Klitna Tetiana
  */
 public class ReportTemplateAmountStudentCourse extends ReportTemplateBuilder{
-
+    
+    private List<Integer> courses;    
+    List reportData = new LinkedList();
+    
+    public ReportTemplateAmountStudentCourse(){
+        DAOReport reportDAO = new DAOReport();
+        courses = reportDAO.getCourses();
+        
+        if(courses == null){
+            courses = new ArrayList<Integer>();
+        }
+        getReport();
+    }
+    
     @Override
     public PdfPCell buildTitle() {
         PdfPCell cell= new PdfPCell();
@@ -36,31 +55,45 @@ public class ReportTemplateAmountStudentCourse extends ReportTemplateBuilder{
 
     @Override
     public PdfPCell buildTable() {
-        String[] header = new String[]{"№", "Фамилия", "Имя", "Телефон"};       
-        
-         float[] size = new float[]{0.5f, 1.5f, 1.5f, 1.5f};
+        String[] header = new String[]{"№", "Фамилия", "Имя", "Email", "Телефон"};  
+                
+         float[] size = new float[]{0.5f, 1.5f, 1.5f, 1.5f, 1.5f};
          DesignTable table = new DesignTableWithGroups(size);
      
          report.setDesignTable(table);
-         PdfPCell cell = report.setTable(header, dataReport(), null);
+         PdfPCell cell = report.setTable(header, reportData, null);
         
   
         return cell;
     }
-
-    @Override
-    public PdfPCell buildChart() {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
+  
 
     @Override
     public List dataReport() {
-        throw new UnsupportedOperationException("Not supported yet.");
+       return reportData;
     }
-
+    
+    @Override
+    public PdfPCell buildChart() {
+        return new PdfPCell();
+    }
+      
     @Override
     public byte[] getChart(int widht, int height) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return null;
     }
+    
+     private void getReport(){
+                   
+        Iterator<Integer> iterator = courses.iterator();
+        while(iterator.hasNext()){
+            
+          Integer course = iterator.next();                           
+          List forms = (new DAOReport()).getFormByCourse(course);           
+                   
+          reportData.add(new Object[]{"" + course, forms});
+                  
+        }
+      }
     
 }
